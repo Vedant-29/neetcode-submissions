3ce6a1b0def09f8990c2a53b7a747f96a9fccc34
Vedant-29 <64523396+Vedant-29@users.noreply.github.com>
2026-05-13 13:43:15 +0530

    Add: python-dynamic-typing - submission-4

---
+++ Python For Beginners/python-dynamic-typing/submission-4.py
@@ -0,0 +1,10 @@
+variable = 10
+print(type(variable))
+variable = 10.22
+print(type(variable))
+variable = True
+print(type(variable))
+variable = "Helllo"
+print(type(variable))
+variable = [1,2,23]
+print(type(variable))
