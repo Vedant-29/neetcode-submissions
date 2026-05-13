2a5409e91227b9a4de67986bc2025e30b9c728d3
Vedant-29 <64523396+Vedant-29@users.noreply.github.com>
2026-05-13 14:05:38 +0530

    Add: python-more-operators - submission-0

---
+++ Python For Beginners/python-more-operators/submission-0.py
@@ -0,0 +1,14 @@
+a, b, c = 2, 8, 5
+
+mul = a*b
+floor_division = mul // c
+
+remainder = mul % c
+
+power_a = a**b
+power_b = b**c
+
+print(floor_division)
+print(remainder)
+print(power_a)
+print(power_b)
